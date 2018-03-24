877371b10454a43534e91216b00fdaeb0b291c38
Dallin Parker <dparker@DPARKER88163M.local>
2018-03-23 22:30:45 -0400

    basic structure to adding menu items

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const electron = require('electron')
 
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow, Menu} = electron
 
 let mainWindow
 
-app.on('ready', () => mainWindow = new BrowserWindow({}))
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({})
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(mainMenu)
+})
+
+const menuTemplate = [
+    //the first object comes from the name of the project, so giving it an empty obj
+    // allows us to see 'File' as it's own top level menu item. set conditionally below
+    // {},
+    {
+        label: 'File',
+        submenu: [
+            {label: 'New Todo'},
+            {
+                label: 'Quit',
+                click() {
+                    app.quit()
+                }
+            }
+        ]
+    }
+]
+
+if(process.platform === 'darwin') {
+    // adding empty obj at the start because we're on an OSX machine
+    menuTemplate.unshift({})
+}
